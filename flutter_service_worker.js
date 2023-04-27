f8126f20c3950dddaf86eb1accf65b1b4983e355
PedroCarlos <91665934+Pedro-Carlos@users.noreply.github.com>
2023-04-27 10:20:58 +0100

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,8 +24,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e62d4db9b940333d2d8b8820a20fcbea",
-"/": "e62d4db9b940333d2d8b8820a20fcbea",
+"index.html": "da1c0c55905169f73f49c7cc1a87427d",
+"/": "da1c0c55905169f73f49c7cc1a87427d",
 "main.dart.js": "fe9cc29a6d06c397e44afa3ae00a8291",
 "manifest.json": "0030ff64be1c3181710c3014b11018a8",
 "version.json": "2b521e10dfa0f067561de489a19d6620"
